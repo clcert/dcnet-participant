ec9565c563e2e2a8f12c932177af21bf704dd901
Camilo Gomez <cagomeznu@gmail.com>
2016-01-13 16:57:24 -0300

    The nodes wait for the rest of the room to be connected to send the first message automatically

--- src/main/java/NodeDCNET.java
+++ src/main/java/NodeDCNET.java
@@ -5,6 +5,7 @@ import org.zeromq.ZThread;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 class NodeDCNET implements ZThread.IAttachedRunnable {
 
@@ -38,7 +39,7 @@ class NodeDCNET implements ZThread.IAttachedRunnable {
         // Read from other nodes
         while (!Thread.currentThread().isInterrupted()) {
             String inputMessage = receiver.recvStr().trim();
-            System.out.println(inputMessage);
+            System.out.println("Message received = " + inputMessage);
         }
 
         receiver.close();
@@ -55,22 +56,61 @@ class NodeDCNET implements ZThread.IAttachedRunnable {
         // Create socket to receive connections from other nodes
         ZMQ.Socket sender = context.createSocket(ZMQ.PUB);
 
+        int nodeIndex;
+
         // Explore in all the ports to bind my connection
-        for (int i = 0; i < dcNetSize; i++) {
+        for (nodeIndex = 0; nodeIndex < dcNetSize; nodeIndex++) {
             try {
-                sender.bind("tcp://*:900" + i);
+                sender.bind("tcp://*:900" + nodeIndex);
             } catch (Exception e) {
                 continue;
             }
-            System.out.println(name + " binded on port 900" + i);
+            System.out.println(name + " binded on port 900" + nodeIndex);
             break;
+        }
+
+        ZMQ.Socket[] repliers = null;
+        ZMQ.Socket[] requesters = null;
+
+        if (nodeIndex != 0) {
+            repliers = new ZMQ.Socket[nodeIndex];
+            for (int i = 0; i < repliers.length; i++) {
+                repliers[i] = context.createSocket(ZMQ.REP);
+                repliers[i].bind("tcp://*:700" + (nodeIndex - 1 + i));
+                System.out.println(name + " opened connection on port 700" + (nodeIndex - 1 + i));
+
+                // First synchronization
+                repliers[i].recv(0);
+                repliers[i].send("", 0);
+                System.out.println("Synchronized!");
+            }
+        }
+
+        if (nodeIndex != dcNetSize - 1) {
+            requesters = new ZMQ.Socket[dcNetSize - nodeIndex - 1];
+            for (int i = 0; i < requesters.length; i++) {
+                requesters[i] = context.createSocket(ZMQ.REQ);
+                requesters[i].connect("tcp://*:700" + (nodeIndex*2 + i)); // <-- Check this with examples with more than 3 nodes!
+                System.out.println(name + " connect to node listening on port 700" + (nodeIndex*2 + i));
+
+                // First synchronization
+                requesters[i].send("".getBytes(), 0);
+                requesters[i].recv(0);
+                System.out.println("Synchronized!");
+            }
         }
 
         // Write to the other nodes
         while (!Thread.currentThread().isInterrupted()) {
+            // Sending first message
+            String outputFirstMessage = "" + new Random().nextInt(100);
+            sender.send(outputFirstMessage);
+            System.out.println("Sent first message to the rest of the DCNET = " + outputFirstMessage);
+
+            // Used to stop the while
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-            String outputMessage = stdIn.readLine();
-            sender.send(name + ": " + outputMessage);
+            stdIn.readLine();
+
         }
 
         context.destroy();
