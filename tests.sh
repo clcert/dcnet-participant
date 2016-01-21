bf1653d7a8df08470bf4e53378d145ed693397fd
Camilo Gomez <cagomeznu@gmail.com>
2016-01-21 16:48:50 -0300

    minimum changes to test script

--- tests.sh
+++ tests.sh
@@ -2,6 +2,5 @@
 #open Terminal and run command
 for ((i=1; i<=$1; i++))
 do
-	#xfce4-terminal -e './gradlew run -PappArgs=['$RANDOM','$1']'
 	./gradlew run -PappArgs=[$RANDOM,$1] > logs/log$i.txt &
 done
