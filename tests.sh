793b5fc19067a2c0b8dfd112ef159f48a123ce2e
Camilo Gomez <cagomeznu@gmail.com>
2016-01-22 16:28:01 -0300

    improved test, waiting until all the threads are over

--- tests.sh
+++ tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-#open Terminal and run command
 for ((i=1; i<=$1; i++))
 do
 	./gradlew run -PappArgs=[$RANDOM,$1] > logs/log$i.txt &
+	pids[$i]=$!
+done
+
+for pid in ${pids[*]}
+do
+	wait $pid
 done
