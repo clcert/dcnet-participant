8a382d979cd6bb27c2c6317bc4759783c1a5a5af
Camilo Gomez <cagomeznu@gmail.com>
2016-03-08 13:00:35 -0300

    not assigning nodeIndex by the node, instead given by the directory

--- src/main/java/NodeDCNET.java
+++ src/main/java/NodeDCNET.java
@@ -25,27 +25,26 @@ class NodeDCNET implements ZThread.IAttachedRunnable {
     private final String myIp;
     private final String name;
     private final int message;
-    private final int nodeIndex;
+    private int nodeIndex;
     private final boolean NONPROBABILISTIC = true;
     private final String directoryIp;
 
-    private static Hashtable<Integer, String> directory = new Hashtable();
+    //private static Hashtable<Integer, String> directory = new Hashtable();
+    private static HashMap<Integer, String> directory = new HashMap<>();
 
-    public NodeDCNET(String myIp, String name, String message, String dcNetSize, String nodeIndex, String directoryIp) {
+    public NodeDCNET(String myIp, String name, String message, String dcNetSize, String directoryIp) {
         this.myIp = myIp;
         this.name = name;
         this.message = Integer.parseInt(message);
         this.dcNetSize = Integer.parseInt(dcNetSize);
-        this.nodeIndex = Integer.parseInt(nodeIndex);
         this.directoryIp = directoryIp;
     }
 
-    // Usage: ./gradlew run -PappArgs=[<message>,<numberOfNodes>,<nodeIndex>,<directoryIP>]
+    // Usage: ./gradlew run -PappArgs=[<message>,<numberOfNodes>,<directoryIP>]
     public static void main(String[] args) throws IOException {
         String myIp = getLocalNetworkIp();
         System.out.println("my IP: " + myIp + "\n");
-
-        new NodeDCNET(myIp, "Node " + args[2], args[0], args[1], args[2], args[3]).createNode();
+        new NodeDCNET(myIp, "Node", args[0], args[1], args[2]).createNode();
     }
 
     // Receiver Thread
@@ -147,6 +146,17 @@ class NodeDCNET implements ZThread.IAttachedRunnable {
         }
         System.out.println("FINISHED directory process");
 
+        // Rescue index (key) of the node given my ip (value)
+        Set directorySet = NodeDCNET.directory.entrySet();
+        for (Object aDirectorySet : directorySet) {
+            Map.Entry mapEntry = (Map.Entry) aDirectorySet;
+            int indexKey = (int) mapEntry.getKey();
+            String ipValue = NodeDCNET.directory.get(indexKey);
+            if (ipValue.equals(myIp))
+                this.nodeIndex = indexKey;
+        }
+        System.out.println("My index is: " + this.nodeIndex);
+
         // We need to connect every pair of nodes in order to synchronize the sending of values at the beginning of each round
         // For this, we need that in every pair of nodes there will be one requestor and one replier
         // In every pair of nodes {i,j} where i<j, node i will work as a requestor and node j will work as a replier
@@ -155,7 +165,7 @@ class NodeDCNET implements ZThread.IAttachedRunnable {
         ZMQ.Socket[] requestors = initializeRequestorsArray(nodeIndex, context);
 
         // Throw receiver thread which runs the method 'run' described above
-        ZMQ.Socket receiverThread = ZThread.fork(context, new NodeDCNET(this.myIp, this.name, "" + this.message, "" + this.dcNetSize, "" + this.nodeIndex, this.directoryIp), myIp);
+        ZMQ.Socket receiverThread = ZThread.fork(context, new NodeDCNET(this.myIp, this.name, "" + this.message, "" + this.dcNetSize, this.directoryIp), myIp);
 
         /*System.out.println("waiting to all nodes be connected");
         // Synchronize Publishers and Subscribers
