3af6cc53689d18a599fed27446009abd41743606
Camilo Gomez <cagomeznu@gmail.com>
2016-05-02 18:05:45 -0300

    testing multiplication on commitments result on one using DH (19) --not solved yet, need to check how to make commitments and modular reduction

--- src/main/java/participantnode/SessionManager.java
+++ src/main/java/participantnode/SessionManager.java
@@ -151,7 +151,6 @@ public class SessionManager {
                 // Calculate and save commitments on each round key
                 BigInteger[] commitmentsOnKeys = new BigInteger[roundKeys.length];
                 for (int i = 0; i < roundKeys.length; i++) {
-                    System.out.println("shared random value: " + sharedRandomValues[i]);
                     commitmentsOnKeys[i] = pedersenCommitment.calculateCommitment(roundKeys[i], sharedRandomValues[i]);
                 }
                 // Generate general commitment value for the resulting round key (operation over round keys)
@@ -172,10 +171,10 @@ public class SessionManager {
                 // Confirm that received all commitments on keys
                 receiverThread.recvStr();
                 // Check that multiplication result is 1
-                if (multiplicationOnCommitments.equals(BigInteger.ONE))
+                if (multiplicationOnCommitments.mod(room.getP()).equals(BigInteger.ONE))
                     System.out.println("Round " + round + " commitments on keys are OK");
                 else
-                    System.out.println("Round " + round + " commitments on keys are WRONG: " + multiplicationOnCommitments.mod(room.getP()));
+                    System.out.println("Round " + round + " commitments on keys are WRONG");
 
                 // Synchronize nodes to let know that we all finish the key commitments part
                 synchronizeNodes(nodeIndex, repliers, requestors, room);
